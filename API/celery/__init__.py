3c4aacfb3f716b91f91ebf25642843fc3e76d5f7
hmze-git <hamzahrassul92@gmail.com>
2026-05-02 23:59:08 +0200

    docker and celery stuff

---
+++ API/celery/__init__.py
@@ -0,0 +1,11 @@
+from celery import Celery
+import os
+
+redis_url= os.getenv("REDIS_URL","redis://redis:6379/0")
+celery = Celery(
+    'celery',
+    broker=redis_url,
+    backend=redis_url,
+)
+
+from . import tasks
